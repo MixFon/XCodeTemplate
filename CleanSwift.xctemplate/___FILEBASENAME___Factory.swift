bbb061832d60e78fb59ecc18208db2f2cbd9d98b
mixfon <mixfon@rambler.ru>
2023-05-05 11:08:52 +0300

    Изменение сонфигуратора на фабрику

--- CleanSwift.xctemplate/___FILEBASENAME___Factory.swift
+++ CleanSwift.xctemplate/___FILEBASENAME___Factory.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-final class ___VARIABLE_sceneName___Configurator {
+final class ___VARIABLE_sceneName___Factory {
 	
 	func configure() -> UIViewController {
-		let controller = ___VARIABLE_sceneName___Controller(nibName: "___VARIABLE_sceneName___Controller", bundle: bundle)
+		let controller = ___VARIABLE_sceneName___Controller(nibName: "___VARIABLE_sceneName___Controller", bundle: <#T##Bundle?#>)
 		let presenter = ___VARIABLE_sceneName___Presenter(controller: controller)
 		let interactor = ___VARIABLE_sceneName___Interactor(presenter: presenter)
 		let router = ___VARIABLE_sceneName___Router(controller: controller)
