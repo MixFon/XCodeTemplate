01967bc2cdbf4c95011c1bd069536dc74277ab76
mixfon <mixfon@rambler.ru>
2024-11-18 13:24:29 +0300

    Добавил новый шаблон для простого SwiftUI экрана

---
+++ CleanSwiftUISimple.xctemplate/___FILEBASENAME___Factory.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import UIKit
+import SwiftUI
+
+final class ___VARIABLE_sceneName___Factory {
+	
+	func configure() -> UIViewController {
+		let mainView = ___VARIABLE_sceneName___View()
+		let controller = UIHostingController(rootView: mainView)
+		return controller
+	}
+}
