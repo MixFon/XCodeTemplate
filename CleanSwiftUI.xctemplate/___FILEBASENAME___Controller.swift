f65ff63893b5e9e420041d890006001e00f425c6
mixfon <mixfon@rambler.ru>
2026-02-02 10:38:26 +0300

    Убрал async

--- CleanSwiftUI.xctemplate/___FILEBASENAME___Controller.swift
+++ CleanSwiftUI.xctemplate/___FILEBASENAME___Controller.swift
@@ -8,10 +8,10 @@
 import UIKit
 import SwiftUI
 
-protocol ___VARIABLE_sceneName___DisplayLogic: AnyObject, Sendable {
-	@MainActor func requestRoute(_ route: ___VARIABLE_sceneName___Model.Route) async
+protocol ___VARIABLE_sceneName___DisplayLogic: AnyObject {
+	@MainActor func requestRoute(_ route: ___VARIABLE_sceneName___Model.Route)
 	func perfromAction(_ action: ___VARIABLE_sceneName___Model.Action) async
-	@MainActor func displayContent(_ viewModel: ___VARIABLE_sceneName___Model.ViewModel) async
+	@MainActor func displayContent(_ viewModel: ___VARIABLE_sceneName___Model.ViewModel)
 }
 
 final class ___VARIABLE_sceneName___Controller: UIHostingController<___VARIABLE_sceneName___View> {
@@ -30,14 +30,14 @@ final class ___VARIABLE_sceneName___Controller: UIHostingController<___VARIABLE_
 
 extension ___VARIABLE_sceneName___Controller: ___VARIABLE_sceneName___DisplayLogic {
     
-	func displayContent(_ viewModel: ___VARIABLE_sceneName___Model.ViewModel) async {
+	func displayContent(_ viewModel: ___VARIABLE_sceneName___Model.ViewModel) {
 		switch viewModel {
 		case .display:
 			break
 		}
 	}
 	
-	func requestRoute(_ route: ___VARIABLE_sceneName___Model.Route) async {
+	func requestRoute(_ route: ___VARIABLE_sceneName___Model.Route) {
 		switch route {
 		case .route:
 			break
